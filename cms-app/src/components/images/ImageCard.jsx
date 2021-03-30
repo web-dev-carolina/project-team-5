176d073be0b0c5fff02cfd24047c56b7b69b82ba
Tatiana Kelliher <59715740+tatianak317@users.noreply.github.com>
2021-03-29 22:08:08 -0500

    Create ImageCard.jsx

---
+++ cms-app/src/components/images/ImageCard.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import Card from "react-bootstrap/Card";
+import { Container, Button, Form, Modal } from "react-bootstrap";
+import Axios from "axios";
+import { useHistory } from "react-router-dom";
+
+const ImageCard = (props) => {
+    return (
+        <>
+        </>
+    );
+}
+
+export default ImageCard;
